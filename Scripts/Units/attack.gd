39e17ca39cc8611bceb3696b854c58ea591f96d9
IRVINEUSD\26linyuhangje <Jeremylinyh@gmail.com>
2025-12-01 11:47:35 -0800

    Update attack.gd

--- Scripts/Units/attack.gd
+++ Scripts/Units/attack.gd
@@ -1,8 +1,23 @@
 extends Node2D
 
 @export var damage : int = 10
+@export var range : float = 6
+@export var swingTime : int = 1
+
+@export var pixaxe : PackedScene
 
 func _input(event):
 	if event.is_action_pressed("Attack") :
-		pass
 		print("Attack!")
+		# Spawn pixaxe
+		var weapon : Node2D = pixaxe.instantiate()
+		
+		# swing
+		var mouse_position : Vector2 = get_viewport().get_mouse_position()
+		var direction : Vector2 = (mouse_position - $"..".position)
+		direction = direction.normalized()
+		
+		var vertexPos : Vector2 = $"..".position + direction
+		
+		var tween = get_tree().create_tween()
+		# destroy pixaxe
